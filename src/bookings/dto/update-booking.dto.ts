422a438190a558516afa5a51e34323e54db6c2d8
Firdhaus_Hardiansyah <fhardiansyah91@gmail.com>
2026-05-26 22:16:19 +0800

    fix booking mechanic dto

--- src/bookings/dto/update-booking.dto.ts
+++ src/bookings/dto/update-booking.dto.ts
@@ -1,5 +1,11 @@
 import { PartialType } from '@nestjs/swagger';
 import { CreateBookingDto } from './create-booking.dto';
+import { IsOptional, IsString } from 'class-validator';
 
 export class UpdateBookingDto extends PartialType(
-    CreateBookingDto) { mechanic?: string; }
+  CreateBookingDto
+) {
+  @IsOptional()
+  @IsString()
+  mechanic?: string;
+}
